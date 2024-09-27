0fb45c43d07546349c80b0ecd6f7e2cd27e842e8
Darmanin96 <darmanindaniel@hotmail.es>
2024-09-27 10:44:32 +0100

    Cambios

--- VentanaConMemoria/src/main/java/dad/VentanaConMemoriaApp.java
+++ VentanaConMemoria/src/main/java/dad/VentanaConMemoriaApp.java
@@ -4,20 +4,26 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
 import javafx.scene.Scene;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+import javafx.scene.Scene;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.control.Slider;
 import javafx.geometry.Pos;
 import javafx.scene.paint.Color;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 
-import java.awt.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.util.Properties;
-
 
 public class VentanaConMemoriaApp extends Application {
 
@@ -25,8 +31,9 @@ public class VentanaConMemoriaApp extends Application {
     private DoubleProperty y = new SimpleDoubleProperty();
     private DoubleProperty with = new SimpleDoubleProperty();
     private DoubleProperty high = new SimpleDoubleProperty();
-
-    private IntegerProperty  red = new SimpleDoubleProperty();
+    private IntegerProperty red = new SimpleIntegerProperty();
+    private IntegerProperty green = new SimpleIntegerProperty();
+    private IntegerProperty blue = new SimpleIntegerProperty();
 
     @Override
     public void init() throws Exception {
@@ -39,70 +46,72 @@ public class VentanaConMemoriaApp extends Application {
 
         if (configFile.exists()) {
             FileInputStream fis = new FileInputStream(configFile);
-         Properties prop = new Properties();
-         prop.load(fis);
-        with.set(Double.parseDouble(prop.getProperty("size.with: ")));
-        high.set(Double.parseDouble(prop.getProperty("size.high: ")));
-        x.set(Double.parseDouble(prop.getProperty("location.x: ")));
-        y.set(Double.parseDouble(prop.getProperty("location.y: ")));
-        }else {
+            Properties prop = new Properties();
+            prop.load(fis);
+            with.set(Double.parseDouble(prop.getProperty("size.width")));
+            high.set(Double.parseDouble(prop.getProperty("size.height")));
+            x.set(Double.parseDouble(prop.getProperty("location.x")));
+            y.set(Double.parseDouble(prop.getProperty("location.y")));
+        } else {
             with.set(320);
             high.set(200);
             x.set(0);
             y.set(0);
         }
-
     }
-
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-        Slider redslider = new Slider();
-        redslider.setMin(0);
-        redslider.setMax(255);
+        Slider redslider = new Slider(0, 255, 0);
         redslider.setShowTickLabels(true);
         redslider.setMajorTickUnit(255);
         redslider.setMinorTickCount(5);
 
-
-        Slider greenslider = new Slider();
-        greenslider.setMin(0);
-        greenslider.setMax(255);
+        Slider greenslider = new Slider(0, 255, 0);
         greenslider.setShowTickLabels(true);
         greenslider.setMajorTickUnit(255);
         greenslider.setMinorTickCount(5);
 
-        Slider blueslider = new Slider();
-        blueslider.setMin(0);
-        blueslider.setMax(255);
+        Slider blueslider = new Slider(0, 255, 0);
         blueslider.setShowTickLabels(true);
         blueslider.setMajorTickUnit(255);
         blueslider.setMinorTickCount(5);
 
-
-        VBox root = new VBox();
-        root.setFillWidth(false);
+        VBox root = new VBox(10);
         root.setAlignment(Pos.CENTER);
         root.getChildren().addAll(redslider, greenslider, blueslider);
 
-        Scene scene = new Scene(root,with.get(),high.get());
+        Scene scene = new Scene(root, with.get(), high.get());
         primaryStage.setX(x.get());
         primaryStage.setY(y.get());
         primaryStage.setTitle("Ventana con memoria");
         primaryStage.setScene(scene);
         primaryStage.show();
 
+
         x.bind(primaryStage.xProperty());
         y.bind(primaryStage.yProperty());
         with.bind(primaryStage.widthProperty());
         high.bind(primaryStage.heightProperty());
 
-        redslider.valueProperty().bindBidirectional(red);
-        red.add(0,av,nv) -> {
-            Color o = Color.rgb(nv.inValue(),0,0);
-            root.setBackground(Background.fill(c));
-        }
+        redslider.valueProperty().addListener((obs, oldVal, newVal) -> {
+            red.set(newVal.intValue());
+            updateBackground(root);
+        });
+
+        greenslider.valueProperty().addListener((obs, oldVal, newVal) -> {
+            green.set(newVal.intValue());
+            updateBackground(root);
+        });
+
+        blueslider.valueProperty().addListener((obs, oldVal, newVal) -> {
+            blue.set(newVal.intValue());
+            updateBackground(root);
+        });
+    }
+
+    private void updateBackground(VBox root) {
+        Color color = Color.rgb(red.get(), blue.get(), green.get());
+        root.setBackground(new Background(new BackgroundFill(color, CornerRadii.EMPTY, null)));
     }
 
     @Override
@@ -113,21 +122,20 @@ public class VentanaConMemoriaApp extends Application {
         File configFolder = new File(profileFolder, ".VentanaConMemoria");
         File configFile = new File(configFolder, "config.properties");
 
-        if (!configFile.exists()) {
+        if (!configFolder.exists()) {
             configFolder.mkdir();
         }
 
-        System.out.println("Profile    : " +  profileFolder);
-        System.out.println("Config folder    : " + configFolder);
-        System.out.println("Config file  : " + configFile);
-
-        FileOutputStream fos = new FileOutputStream("config.properties");
-
-        Properties props = new Properties();
-        props.setProperty("size width: ","" + with.get());
-        props.setProperty("size height: ","" + high.get());
-        props.setProperty("location x: ","" + x.get());
-        props.setProperty("location y: ","" + y.get());
-        props.store(fos,"Estado de la ventana");
+        try (FileOutputStream fos = new FileOutputStream(configFile)) {
+            Properties props = new Properties();
+            props.setProperty("size.width", String.valueOf(with.get())); // Corrigiendo nombre de propiedad
+            props.setProperty("size.height", String.valueOf(high.get())); // Corrigiendo nombre de propiedad
+            props.setProperty("location.x", String.valueOf(x.get())); // Corrigiendo nombre de propiedad
+            props.setProperty("location.y", String.valueOf(y.get())); // Corrigiendo nombre de propiedad
+            props.store(fos, "Estado de la ventana");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
